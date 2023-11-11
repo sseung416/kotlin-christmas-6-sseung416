9a35a33496f4dab92cf2239dd11329b65ca43aa1
sseung416 <choiseungyeon416@gmail.com>
2023-11-11 12:37:10 +0900

    test: DiscountTest의 잘못 작성된 메서드명 수정
    평일의 기준이 잘못 작성되어 있었음

--- src/test/kotlin/christmas/domain/DiscountTest.kt
+++ src/test/kotlin/christmas/domain/DiscountTest.kt
@@ -6,15 +6,15 @@ import org.junit.jupiter.api.Test
 class DiscountTest {
 
     @Test
-    fun `평일(월~목)에 디저트 메뉴 2개 주문했을 때, 4,046원을 할인해준다`() {
-        val menu = Menu("${Dessert.ChocolateCake.menuName}-2")
+    fun `평일(일~목)에 디저트 메뉴 2개 주문했을 때, 4,046원을 할인해준다`() {
+        val menu = Menu(menuInput = "${Dessert.ChocolateCake.menuName}-2")
         val discount = WeekdayDiscount(visitDate = VisitDate.from("4"), menu = menu)
         assertThat(discount.amount).isEqualTo(DISCOUNT_AMOUNT * 2)
     }
 
     @Test
-    fun `평일(월~목)에 메인 메뉴 1개를 주문했을 때, 할인 금액은 없다`() {
-        val menu = Menu("${MainDish.BarbecueRibs.menuName}-1")
+    fun `평일(일~목)에 메인 메뉴 1개를 주문했을 때, 할인 금액은 없다`() {
+        val menu = Menu(menuInput = "${MainDish.BarbecueRibs.menuName}-1")
         val discount = WeekdayDiscount(visitDate = VisitDate.from("4"), menu = menu)
         assertThat(discount.amount).isEqualTo(0)
     }
