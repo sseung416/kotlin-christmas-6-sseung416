7f07aedfc3ec82c564431ce73dcd20740f9baa53
sseung416 <choiseungyeon416@gmail.com>
2023-11-11 10:06:56 +0900

    feat: 입력 값을 메뉴 목록으로 변환 후 메뉴 입력 관련 로직 검증 구현
    메뉴 개수, 메뉴 주문 형식, 메뉴 중복, 메뉴 내역에 대한 검증

---
+++ src/main/kotlin/christmas/domain/Menu.kt
@@ -0,0 +1,46 @@
+package christmas.domain
+
+class Menu(menuInput: String) {
+
+    private val countByMenuItem: Map<MenuItem, Int>
+
+    init {
+        val pairs = menuInput.split(DELIMITER).map { menuNameAndCountinput ->
+            runCatching {
+                val list = menuNameAndCountinput.split(SEPARATOR)
+                MenuItem.from(list[0]) to list[1].toValidInt(Error.InvalidMenu.message)
+            }.getOrElse {
+                throw IllegalArgumentException(Error.InvalidMenu.message)
+            }
+        }
+
+        val (menuItems, counts) = pairs.unzip()
+        validateDuplicatedMenu(menuItems)
+        validateCount(counts)
+        validateMaxOrderCount(counts)
+        validateOnlyDrink(menuItems)
+
+        countByMenuItem = pairs.associate { it }
+    }
+
+    private fun validateDuplicatedMenu(menuItems: List<MenuItem>) {
+        require(menuItems.toSet().size == menuItems.size) { Error.InvalidMenu.message }
+    }
+
+    private fun validateCount(counts: List<Int>) {
+        require(counts.all { it >= 1 }) { Error.InvalidMenu.message }
+    }
+
+    private fun validateMaxOrderCount(count: List<Int>) {
+        require(count.sum() <= 20) { Error.InvalidMenu.message }
+    }
+
+    private fun validateOnlyDrink(menuItems: List<MenuItem>) {
+        require(menuItems.any { it !is Drink }) { Error.InvalidMenu.message }
+    }
+
+    companion object {
+        private const val DELIMITER = ","
+        private const val SEPARATOR = "-" // TODO: rename
+    }
+}
---
+++ src/test/kotlin/christmas/domain/MenuTest.kt
@@ -0,0 +1,38 @@
+package christmas.domain
+
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+class MenuTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = ["양송이스프-0", "타파스-1,제로콜라-0"])
+    fun `메뉴는 1개 이상 주문이 가능하다`(input: String) {
+        assertThatThrownBy { Menu(input) }.hasError(Error.InvalidMenu)
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["양송이수프-21", "타파스-10,초코케이크-5,제로콜라-10", "티본스테이크-10000000000000000000000000"])
+    fun `최대 주문 가능 개수(20개)를 넘긴 경우, 오류를 반환한다`(input: String) {
+        assertThatThrownBy { Menu(input) }.hasError(Error.InvalidMenu)
+    }
+
+    @Test
+    fun `중복된 메뉴를 입력할 경우, 오류를 반환한다`() {
+        assertThatThrownBy { Menu("양송이수프-1, 양송이수프-1") }.hasError(Error.InvalidMenu)
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["제로콜라-1", "레드와인-1", "샴페인-1"])
+    fun `음료만 주문할 경우, 오류를 반환한다`(input: String) {
+        assertThatThrownBy { Menu(input) }.hasError(Error.InvalidMenu)
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["", "안녕", "양송이수프-2,타파스", "타파스-1.양송이수프-1"])
+    fun `입력 값이 메뉴의 주문 형식이 다른 경우, 오류를 반환한다`(input: String) {
+        assertThatThrownBy { Menu(input) }.hasError(Error.InvalidMenu)
+    }
+}
