3fb6dacf676d72dd47ee6d3258512994683d81a2
sseung416 <choiseungyeon416@gmail.com>
2023-11-11 12:37:36 +0900

    test: VisitDate의 isWeekend, isWeekday 값 테스트 코드 작성

--- src/test/kotlin/christmas/domain/VisitDateTest.kt
+++ src/test/kotlin/christmas/domain/VisitDateTest.kt
@@ -1,5 +1,6 @@
 package christmas.domain
 
+import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
@@ -16,5 +17,19 @@ class VisitDateTest {
     @ValueSource(strings = ["0", "100", "32", "111111111111111111111"])
     fun `입력 값이 1~31의 숫자가 아닐 때, 오류를 반환한다`(input: String) {
         assertThatThrownBy { VisitDate.from(input) }.hasError(Error.InvalidDate)
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["1", "2", "8", "9"])
+    fun `예약 날짜로 주말을 입력했을 때, isWeekend의 값이 true이다`(input: String) {
+        val visitDate = VisitDate.from(input)
+        assertThat(visitDate.isWeekend).isTrue()
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["4", "11", "5", "6", "7"])
+    fun `예약 날짜로 평일을 입력했을 때, isWeekday의 값이 true이다`(input: String) {
+        val visitDate = VisitDate.from(input)
+        assertThat(visitDate.isWeekday).isTrue()
     }
 }
