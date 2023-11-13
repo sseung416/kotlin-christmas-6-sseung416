4f737eff8b7065b127fb29492ef080976af89dc0
sseung416 <choiseungyeon416@gmail.com>
2023-11-13 10:48:53 +0900

    feat: Application 연동

---
+++ src/main/kotlin/christmas/controller/ChristmasEventController.kt
@@ -0,0 +1,36 @@
+package christmas.controller
+
+import christmas.domain.Menu
+import christmas.domain.Receipt
+import christmas.domain.VisitDate
+import christmas.view.InputView
+import christmas.view.OutputView
+
+class ChristmasEventController(
+    private val outputView: OutputView,
+    private val inputView: InputView
+) {
+
+    fun start() {
+        outputView.printIntroduce()
+        val visitDate = getVisitDate()
+        val menu = getMenu()
+        outputView.printReceipt(Receipt(visitDate, menu))
+    }
+
+    private fun getVisitDate(): VisitDate =
+        try {
+            VisitDate.from(inputView.readVisitDate())
+        } catch (exception: IllegalArgumentException) {
+            outputView.printError(exception.message)
+            getVisitDate()
+        }
+
+    private fun getMenu(): Menu =
+        try {
+            Menu(inputView.readInputMenu())
+        } catch (exception: IllegalArgumentException) {
+            outputView.printError(exception.message)
+            getMenu()
+        }
+}
