c80aa30d15e2846c999a044a105afcdf38ba523b
sseung416 <choiseungyeon416@gmail.com>
2023-11-15 23:36:51 +0900

    test: DecemberEventTest givenWhenThen 형식으로 리팩토링

--- src/test/kotlin/christmas/domain/DecemberEventTest.kt
+++ src/test/kotlin/christmas/domain/DecemberEventTest.kt
@@ -13,41 +13,58 @@ class DecemberEventTest {
 
     @Test
     fun getBadgeNameOrTest() {
+        // given
         val menu = Menu.of("${MainDish.ChristmasPasta.menuName}-10")
         val visitDate = VisitDate.from("1")
         val decemberEvent = DecemberEvent(visitDate, menu)
-        assertThat(decemberEvent.getBadgeNameOr("")).isEqualTo(Badge.Santa.badgeName)
+
+        // when
+        val badgeName = decemberEvent.getBadgeNameOr("")
+
+        // then
+        assertThat(badgeName).isEqualTo(Badge.Santa.badgeName)
     }
 
     @Test
     fun totalBenefitTest() {
+        // given
         val menu = Menu.of("${MainDish.TBoneSteak.menuName}-10,${MainDish.ChristmasPasta.menuName}-1")
         val visitDate = VisitDate.from("25")
         val decemberEvent = DecemberEvent(visitDate, menu)
 
+        // when, then
         val expectedBenefit = Drink.Champagne.price + 3400 + 1000
         assertThat(decemberEvent.totalBenefit).isEqualTo(expectedBenefit)
     }
 
     @Test
     fun getMenuNameAndeCountTest() {
+        // given
         val soupCount = 1
         val menu = Menu.of("${Appetizer.MushroomSoup.menuName}-$soupCount")
         val visitDate = VisitDate.from("1")
         val decemberEvent = DecemberEvent(visitDate, menu)
 
-        assertThat(decemberEvent.getMenuNameAndCount())
+        // when
+        val menuName = decemberEvent.getMenuNameAndCount()
+
+        // then
+        assertThat(menuName)
             .containsKey(Appetizer.MushroomSoup.menuName)
             .containsValue(soupCount)
     }
 
     @Test
     fun getOrGiftNameOr() {
+        // given
         val menu = Menu.of("${Appetizer.MushroomSoup.menuName}-1")
         val visitDate = VisitDate.from("1")
         val decemberEvent = DecemberEvent(visitDate, menu)
 
-        assertThat(decemberEvent.getGiftNameOr(NOTHING)).isEqualTo(NOTHING)
+        // when
+        val giftName = decemberEvent.getGiftNameOr(NOTHING)
+
+        assertThat(giftName).isEqualTo(NOTHING)
     }
 
     companion object {
